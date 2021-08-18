3ef53be88c3da83aed113c73642e51b5cfed966a
Kai <human2lkai@gmail.com>
2021-08-18 19:33:04 +1000

    change database from back4app to Deta, because of GFW

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import { useState } from "react";
-import Parse from "parse/dist/parse.min.js";
+import { Deta } from "deta";
 
-// Your Parse initialization configuration goes here
-const PARSE_APPLICATION_ID = "RZf7WejpHL1PBAMizHgZ7h0IdGusvsMuzqARzvEk";
-const PARSE_HOST_URL = "https://parseapi.back4app.com/";
-const PARSE_JAVASCRIPT_KEY = "wZjOVweHgNmig8JWCtTbBiDVkDdES9Rx3ioqY1cu";
-Parse.initialize(PARSE_APPLICATION_ID, PARSE_JAVASCRIPT_KEY);
-Parse.serverURL = PARSE_HOST_URL;
+const deta = Deta("c08ztmvr_VzzQTNHLfBGn1r7UYAnYTP4Nd1pCwKXv");
+// name your DB
+const db = deta.Base("humans");
 
 function App() {
   const [message, setMessage] = useState("");
@@ -18,31 +15,14 @@ function App() {
   };
 
   async function addMessage() {
-    try {
-      // create a new Parse Object instance
-      const Message = new Parse.Object("Message");
-      // define the attributes you want for your Object
-      Message.set("content", message);
-      // save it on Back4App Data Store
-      await Message.save();
-      alert("Message saved!");
-    } catch (error) {
-      console.log("Error saving new Message: ", error);
-    }
+    db.put({
+      name: message,
+      title: "",
+      has_visor: true,
+    });
   }
 
-  async function fetchMessage() {
-    // create your Parse Query using the Message Class you've created
-    const query = new Parse.Query("Message");
-    // use the equalTo filter to look for user which the name is John. this filter can be used in any data type
-    query.equalTo("content", message);
-    // run the query
-    const Message = await query.first();
-    // access the Parse Object attributes
-    console.log("Message: ", Message.get("content"));
-    console.log("Message id: ", Message.id);
-    setMessage(Message.content);
-  }
+  async function fetchMessage() {}
 
   return (
     <div>
