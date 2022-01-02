cdebe4d5f11fcaa444c8ea4435d2ed8d2c7a59ef
Kai <human2lkai@gmail.com>
2022-01-03 00:27:42 +1100

    site suspended

--- src/index.js
+++ src/index.js
@@ -5,7 +5,10 @@ import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import App from "./App";
 
 ReactDOM.render(
-  <React.StrictMode>{/* <App /> */}</React.StrictMode>,
+  <React.StrictMode>
+    {/* <App /> */}
+    <p></p>
+  </React.StrictMode>,
   document.getElementById("root")
 );
 
