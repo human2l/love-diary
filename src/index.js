bed3e1196a6a3860b42b65cbf3591a7146a68dc0
Kai <human2lkai@gmail.com>
2022-03-03 15:28:30 +1100

    commented out serviceWorkerRegistration

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+// import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 import App from "./App";
 
